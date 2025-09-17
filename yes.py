197c92afca3b55b7db72d5ee2d68e5eef105e7f1
vova hol <vovakholiavko@gmail.com>
2025-09-17 11:54:26 +0100

    add:progrmas

---
+++ yes.py
@@ -0,0 +1,69 @@
+def start():
+ print("hello choose options between 1 to 3")
+ global choice
+ global availability
+ availability = [ "0","2","3","1"]
+ while True:
+  try:
+   choice = int(input())
+   break 
+  except ValueError:
+   print("Error occured")
+   
+ if choice in (1, 2, 3):
+         validation()
+ while choice not in (1,2,3):
+         print("error there is no such an option")
+         start()
+    
+def validation():
+    
+    print("are you sure youd like number",choice,"?")
+    print("Please type yes or no")   
+    
+    response = input()
+    if response.lower() == ("yes"):
+        purchase()
+    elif response.lower() == ("no"):
+          print("please select desired option")
+          start()
+          
+    else:
+        print("Sorry error occured we are bringing you back to the main menu")
+        start()
+def purchase():
+    value = ["0", "12", "15.5", "20"]
+    print("that will cost", value[choice],"£")
+    while True:
+     try:
+       payment = float(input())
+       break
+     except:
+         print("Incorrect datatype please type integers or float numbers")
+         print("that will cost", value[choice],"£")
+        
+    if payment < float(value[choice]):
+        print("insufficient balance")
+        validation()
+     
+    elif payment>=float(value[choice]):
+       result = payment - float(value[choice])
+       print("your change is",round(result, 3), "£")
+       print("product have been disposed have a nice day")
+       availability[choice]=int(availability[choice]) - 1
+       print(availability[choice],"products left") 
+start()
+
+
+
+          
+
+    
+          
+             
+
+
+
+
+
+     
