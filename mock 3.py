24e8e79a84ad39766a9259dc3e7f373fd11e61f2
vova hol <vovakholiavko@gmail.com>
2025-11-19 12:01:24 +0000

    commit new commit

--- mock 3.py
+++ mock 3.py
@@ -11,37 +11,61 @@
 def validation(inputs):
     try:
      int(inputs)
-     return inputs
+     return int(inputs)
     except:
-     print("sorry wrong data type try again, try again")
+     print("sorry wrong data type try again")
      while True:
           try:
               inputs = int(input())
               return inputs
           except ValueError:
-            print("sorry wrong data type try again, try again")
+            print("sorry wrong data type try again")
 
-options = {"Lamborghini Gallardo": 59,
-           "Lamborghini Huracan": 59,
-           "Ferrari F40": 39,
-           "Porsche Boxster":39,
-           "Audi A5": 39,
-           "BMW I8":39,
-           "Lotus Elise":30}
+options = {"Lamborghini Gallardo": {"price":59, "ID":1},
+           "Lamborghini Huracan": {"price":59,  "ID":2}}
+
+
+#options = {"Lamborghini Gallardo": 59, #59£
+           #"Lamborghini Huracan": 59, #59£
+           #"Ferrari F40": 39, #39£
+           #"Porsche Boxster":39,#39£
+           #"Audi A5": 39, #39£
+           #"BMW I8":39, #39£
+           #"Lotus Elise":30} #30£
 
 name = input("please enter your name:")
 adress = input("Enter your adress:")
 
 
 phone_num = validation(input("Enter your phone number:"))
+print(type(phone_num), phone_num)
 NumCarsToDrive = validation(input("enter the amount of cars you'd like to drive, maximum 5:"))
 
-while int(NumCarsToDrive)<5:
+while NumCarsToDrive<5:
     print("Cannot drive more than 5 cars")
     NumCarsToDrive = validation(input("enter the amount of cars you'd like to drive, maximum 5:"))
+user_input = {1: "Lamborghini Gallardo"}
+
+CarNum = 0
+count = 0
+chosen_cars = []
 for keys, value in options.items():
-    print(keys)
-CarsToDrive =input("which of the following cars would you like to drive?:")
-additional_laps = input("Would you like additional laps? yes/no:")
+    count += 1
+    print(f"{count}.",keys)
+while CarNum < NumCarsToDrive:
+   CarsToDrive = validation(input("which of the following cars would you like to drive? please enter a number:"))
+   if CarsToDrive>count or CarsToDrive<=0:
+       print("there is no such an option please try again")
+       continue
+   else:
+        print("your choice has been accepted")
+        CarNum +=1
+        for car, info in options.items():
+            if info["ID"] == CarsToDrive:
+                chosen_cars.append(car)
+                print(chosen_cars)
+            
+        print(CarNum,"your option is", CarsToDrive)
+additional_laps = input("Would you like additional laps? yes/no:") #additional laps 15£
 num_aditional_laps = validation((input("enter the amount of additional laps:")))
 
