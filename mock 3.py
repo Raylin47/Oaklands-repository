f8fa548135d208606d8a9ff131b13828d8bcd74e
vova hol <vovakholiavko@gmail.com>
2025-11-24 13:17:52 +0000

    Final commit

--- mock 3.py
+++ mock 3.py
@@ -1,13 +1,4 @@
-#inputs - name, adresses, phone number, number of cars the customer would like to drive maximum 5, which cars the customer would like to drive, the number of additional laps.
-#Output - Customer details, breakdown of costs, the total cost of experience.
-#information - additional lap = 15£,
-#Lamborghini Gallardo = 59
-#Lamborghini Huracan = 59
-#Ferrari F40 = 49
-#Porsche Boxster = 39
-# Audi A5 = 39
-# BMW i8 = 39
-# Lotus Elise = 30
+
 def validation(inputs):
     try:
      return int(inputs)
@@ -20,36 +11,38 @@ def validation(inputs):
            except ValueError:
                print("sorry wrong data type try again")
 def CarOptions():
-    global count
     count = 0
     for keys in options.keys():
      count += 1
      print(f"{count}.",keys)
-
-
+    return count
+def calculation():
+    global CarCost
+    CarCost = 0
+    for car, price in chosen_cars.items():
+      CarCost += price["price"]
+      print(car, price["price"],"£")
+       
 options = {"Lamborghini Gallardo": {"price":59, "ID":1},
-           "Lamborghini Huracan": {"price":59,  "ID":2}}
-
-#options = {"Lamborghini Gallardo": 59, #59£
-           #"Lamborghini Huracan": 59, #59£
-           #"Ferrari F40": 39, #39£
-           #"Porsche Boxster":39,#39£
-           #"Audi A5": 39, #39£
-           #"BMW I8":39, #39£
-           #"Lotus Elise":30} #30£
+           "Lamborghini Huracan": {"price":59,  "ID":2},
+           "Ferrari F40": {"price":39, "ID":3},
+           "Porsche Boxster": {"price":39, "ID":4},
+           "Audi A5": {"price":39, "ID":5},
+           "BMW I8": {"price":39, "ID":6},
+           "Lotus Elise": {"price":30, "ID":7}
+           }
 
 name = input("please enter your name:")
 adress = input("Enter your adress:")
 
 
 phone_num = validation(input("Enter your phone number:"))
-print(type(phone_num), phone_num)
 NumCarsToDrive = validation(input("enter the amount of cars you'd like to drive, maximum 5:"))
 
 while NumCarsToDrive>5 or 0>=NumCarsToDrive:
     print("Cannot drive more than 5 cars")
     NumCarsToDrive = validation(input("enter the amount of cars you'd like to drive, maximum 5:"))
-user_input = {1: "Lamborghini Gallardo"}
+
 
 CarNum = 0
 chosen_cars = {}
@@ -57,7 +50,7 @@ chosen_ids = []
 while CarNum < NumCarsToDrive:
    CarOptions()
    CarsToDrive = validation(input("which of the following cars would you like to drive? please enter a number:"))
-   if CarsToDrive>count or CarsToDrive<=0:
+   if CarsToDrive>CarOptions() or CarsToDrive<=0:
        print("there is no such an option please try again")
        continue
    for ID in chosen_cars.values():
@@ -71,6 +64,20 @@ while CarNum < NumCarsToDrive:
                 chosen_cars.update({car:{"price":info["price"], "ID":info["ID"]}})
                 CarNum +=1
                 print("Thats your choice number", CarNum,"the option you have chosen is", CarsToDrive, car)
-additional_laps = input("Would you like additional laps? yes/no:") #additional laps 15£
-num_aditional_laps = validation((input("enter the amount of additional laps:")))
+while True:
+    additional_laps = input("Would you like additional laps? yes/no:")
+    if additional_laps.lower() == "yes":   
+     num_aditional_laps = validation((input("enter the amount of additional laps:")))
+     lap_cost = 15
+     calculation()
+     print(f"Additional laps {lap_cost} £")
+     result = CarCost + lap_cost
+     break
+    elif additional_laps.lower() == "no":
+     calculation()
+     result = CarCost
+     break
+    else:
+     print("there is no such an option please try again")
+print(f"Total cost {result}£")
 
