2f263494a9bd3a0e91515beb7b6a3050a5c9d684
vova hol <vovakholiavko@gmail.com>
2025-12-15 13:05:54 +0000

    final commit

--- parcels mock.py
+++ parcels mock.py
@@ -10,6 +10,17 @@ def digit_validation(message, Input):
        return int(Input)
      except:
          print("wrong datatype try again")
+def float_validation(message, Input):
+   try:
+    return float(Input)
+   except:
+    print("wrong datatype try again")
+   while True:
+     try:
+       Input = float(input(message))
+       return float(Input)
+     except:
+         print("wrong datatype try again")  
 def yesNoCondition(message, Input, boolean = None):
  while True:
     if Input.lower() == "yes":
@@ -24,18 +35,15 @@ def yesNoCondition(message, Input, boolean = None):
         print("input is incorrect, please type yes or no")
         Input = input(message)
         continue
-
-ParcelType = {"small":{"size":95, "weight":2, "price":5,},
-       "medium":{"size":150, "weight":15, "price":20,},
-       "large":{"size":450, "weight"30:, "price":30,}}
+result = 0
 count = 1
 ParcelsInfo = {}
 messages = {"name":"Greetings could you enter your name?:",
             "adress":"Please enter your adress:",
             "phone number":"Please enter your phone number:",
             "Parcel number":"Please enter the number of parcels to collect:",
-            "Signature": "Do you want a signature on delivery? yes/no",
-            "tracking": "Do you want a parcel tracking? yes/no"}
+            "Signature": "Do you want a signature on delivery for each parcel? yes/no:",
+            "tracking": "Do you want a parcel tracking for each parcel? yes/no:"}
 
 name = input(messages["name"])
 adress = input(messages["adress"])
@@ -53,13 +61,37 @@ while True:
         break
 
 while parcels_num >= count:
-    height = digit_validation(f"Please enter height of the parcel number {count}:",input(f"Please enter height of the parcel number {count}:"))
-    length = digit_validation(f"Please enter lenth of the parcel number {count}:",input(f"Please enter lenth of the parcel number {count}:"))
-    width = digit_validation(f"Please enter width of the parcel number {count}:",input(f"Please enter width of the parcel number {count}:"))
-    weight = digit_validation(f"Please enter the weight of your parcel number {count}:",Input = input(f"Please enter the weight of your parcel number {count}:"))
-    EnsureData = yesNoCondition(Input = input(f"Are you sure that the following data for parcel number {count} is correct? yes/no:"), message  = "Are you sure that the following data for parcel number {count} is correct?yes/no:")
+    height = float_validation(f"Please enter height in centimeters of the parcel number {count}:",input(f"Please enter height in centimeters of the parcel number {count}:"))
+    length = float_validation(f"Please enter length in centimeters of the parcel number {count}:",input(f"Please enter length in centimeters of the parcel number {count}:"))
+    width = float_validation(f"Please enter width in centimeters of the parcel number {count}:",input(f"Please enter width in centimeters of the parcel number {count}:"))
+    weight = float_validation(f"Please enter the weight in killograms of your parcel number {count}:",Input = input(f"Please enter the weight in killograms of your parcel number {count}:"))
+    EnsureData = yesNoCondition(Input = input(f"Are you sure that the following data for parcel number {count} is correct? yes/no:"), message  = f"Are you sure that the following data for parcel number {count} is correct?yes/no:")
     if EnsureData == False:
         continue
     elif EnsureData == True:
-        ParcelsInfo.update({f"parcel {count}":{"height":height, "length":length,"width":width,"weight":weight}})
+        size = width + length + height
+        if size <= 95 and weight <= 2 :
+          ParcelsInfo.update({f"parcel {count}":{"height":height, "length":length,"width":width,"weight":weight, "type":"small", "price":5.00}})
+        elif size <= 150 and weight <= 15:
+            ParcelsInfo.update({f"parcel {count}":{"height":height, "length":length,"width":width,"weight":weight, "type":"medium", "price":20.00}})
+        elif size <= 450 and weight <= 30:
+            ParcelsInfo.update({f"parcel {count}":{"height":height, "length":length,"width":width,"weight":weight, "type":"large", "price":30.00}})
+        else:
+            print("sorry the data you have entered for the parcel is not acceptable")
+            continue
         count = count + 1
+Signature = yesNoCondition(messages["Signature"], input(messages["Signature"]))
+tracking = yesNoCondition(messages["tracking"], input(messages["tracking"]))
+print(f"Name:{name}")
+print(f"Adress:{adress}")
+print(f"Phone number:{phone_num}")
+for keys, value in ParcelsInfo.items():
+    result += value["price"]
+    print(keys,"individual price: " + str(value["price"]) + "£,","weight of parcel: " + str(value["weight"]) + "kg,","height of parcel: " +  str(value["height"]) + "cm,","length of parcel: " +  str(value["length"]) + "cm,","width of parcel: " +  str(value["width"]) + "cm,", "Parcel type: " + value["type"])
+    if Signature == True:
+       result = result + 2.00
+       print(f"additional signature price for {keys}: +2£")
+    if tracking == True:
+      result = result + 5.00
+      print(f"additional tracking price for {keys}: +5£")
+print(f"Overall cost: {result}£")
